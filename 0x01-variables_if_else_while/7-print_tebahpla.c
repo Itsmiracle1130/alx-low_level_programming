a67c44caacb69c6cbb7f0638881963c6a57afecb
Itsmiracle1130 <meboy4real@gmail.com>
2022-12-09 16:17:12 -0800

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
 putchar('\n');
 return (0);
 }
